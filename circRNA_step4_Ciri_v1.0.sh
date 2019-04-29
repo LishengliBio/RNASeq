d132d4ba0b8900376e055fa2660bc1f8f3e6f4c0
LishengliBio <35823565+LishengliBio@users.noreply.github.com>
2019-04-29 17:22:37 -0500

    Update circRNA_step4_Ciri_v1.0.sh

--- circRNA_step4_Ciri_v1.0.sh
+++ circRNA_step4_Ciri_v1.0.sh
@@ -9,12 +9,4 @@ fa='/extraspace/sli/ref/fa/gencode_v19_genome_hg19.fa'
 gtf='/extraspace/sli/ref/gtf/gencode_v19_annotation_hg19.gtf'
 ciri_out=${dir_ciri}/${sample}/${sample}'_circrna.ciri'
 perl ${ciri2} -T 6 -I ${unmap_bwa_sam} -O ${ciri_out} -F ${fa} -A ${gtf} -G ${dir_ciri}/${sample}/${sample}'_ciri.log'
-star='/extraspace/sli/softwares/bin/STAR'
-gdir='/extraspace/sli/ref/fa/star_index_hg19'
-unmap_fq=${dir_findcirc}/${sample}/${sample}'_unmapped.fq'
-${star} --genomeDir ${gdir} --readFilesIn ${unmap_fq} --runThreadN 15 --chimSegmentMin 20 --chimScoreMin 1 --alignIntronMax 100000 \
---chimOutType Junctions SeparateSAMold --outFilterMismatchNmax 4 --alignTranscriptsPerReadNmax 100000 --outFilterMultimapNmax 2 --outFileNamePrefix ${outdir}/${sample}
-pstalign='/extraspace/sli/softwares/circRNA_finder/postProcessStarAlignment.pl'
-star_align=${dir_ciri}/${sample}
-circ_finder_out=${dir_ciri}/${sample}
-perl ${pstalign} ${star_align} ${circ_finder_out}
+
