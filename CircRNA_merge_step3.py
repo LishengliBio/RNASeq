a63317be17636310562f9b3ec23ab57b01c0852e
LishengliBio <35823565+LishengliBio@users.noreply.github.com>
2019-05-09 10:21:00 -0500

    Update CircRNA_merge_step3.py

--- CircRNA_merge_step3.py
+++ CircRNA_merge_step3.py
@@ -1,1 +1,21 @@
+import re
+import numpy as np
+dir_wk = '/extraspace/sli/collaboration/XiangYu_circRNA/siPHAX/circRNA/merge'
+samples = ['siFFL_repA','siFFL_repB','siFFL_repC','siPHAX_repA','siPHAX_repB','siPHAX_repC']
+for s in samples:
+        print('Processing '+s)
+        file_s = dir_wk+'/'+s+'_tools_circs.txt'
+        fh_s = open(file_s,'r')
+        file_out = dir_wk+'/'+s+'_tools_circs_filter.txt'
+        fh_out = open(file_out,'w')
+        fh_out.write('CircRNA\tMean_reads\n')
+        fh_s.readline()
+        for l in fh_s:
+                l = l.strip()
+                tmp = l.split('\t')
+                if re.search(';',tmp[1]):
+                        Read_mean = int(np.mean([int(n) for n in tmp[2:]]))
+                        fh_out.write(tmp[0]+'\t'+str(Read_mean)+'\n')
+        fh_s.close()
+        fh_out.close()
 
