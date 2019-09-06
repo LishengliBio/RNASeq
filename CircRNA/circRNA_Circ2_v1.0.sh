2d469cd6433a3acd5c681514b0ce5cf052e8bac7
LishengliBio <35823565+LishengliBio@users.noreply.github.com>
2019-09-06 17:15:00 -0500

    Update circRNA_Circ2_v1.0.sh

--- CircRNA/circRNA_Circ2_v1.0.sh
+++ CircRNA/circRNA_Circ2_v1.0.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# Get the sample name from command line
 sample=$1
-# Set working directories and files
-dir_findcirc='/extraspace/sli/collaboration/XiangYu_circRNA/TGF_treat/circRNA/FindCirc'
-dir_circ='/extraspace/sli/collaboration/XiangYu_circRNA/TGF_treat/circRNA/Circ'
+dir_alignment='/extraspace/sli/circRNA01/alignment_ht2'
+dir_circ='/extraspace/sli/circRNA01/circRNA/Circ'
 mkdir -p ${dir_circ}/${sample}
-fa='/extraspace/sli/ref/fa/gencode_v19_genome_hg19.fa'
-unmap_fq=${dir_findcirc}/${sample}/${sample}'_unmapped.fastq'
-unmap_bwa_sam=${dir_circ}/${sample}/${sample}'_unmapped_bwa.sam'
-ann_ref='/extraspace/sli/ref/Circ2_hg19_ref_ann.txt'
-
-# Prepare softwares and tools
+fa='/extraspace/sli/ref/fa/gencode_v28_genome.fa'
 bwa='/extraspace/sli/softwares/bin/bwa'
-circ2='/extraspace/sli/softwares/Anaconda3/bin/CIRCexplorer2'
-
-# Run Circ2 to call circRNAs
-${bwa} mem -t 8 -T 19 ${fa} ${unmap_fq} > ${unmap_bwa_sam} 2> ${outdir}/${sample}'_bwa.log'
+unmap_fq=${dir_alignment}/${sample}/${sample}'_unmapped.fastq'
+unmap_bwa_sam=${dir_circ}/${sample}/${sample}'_unmapped_bwa.sam'
+${bwa} mem -t 8 -T 19 ${fa} ${unmap_fq} > ${unmap_bwa_sam} 2> ${dir_circ}/${sample}/${sample}'_bwa.log'
+circ2='/extraspace/sli/softwares/anaconda2/bin/CIRCexplorer2'
+unmap_bwa_sam=${dir_circ}/${sample}/${sample}'_unmapped_bwa.sam'
 ${circ2} parse -t BWA -b ${dir_circ}/${sample}/${sample}'_circ2_result.txt' ${unmap_bwa_sam} > ${dir_circ}/${sample}/${sample}'_test.parse.log'
+ann_ref='/extraspace/sli/ref/hg38_ref_Circ2.txt'
 ${circ2} annotate -r ${ann_ref} -g ${fa} -b ${dir_circ}/${sample}/${sample}'_circ2_result.txt' -o ${dir_circ}/${sample}/${sample}'_circ2_result_ann.circ2'
-
-## Note: you can download the annotation file using circRNA_circ_fetch_ann.py
-# python fetch_ucsc.py hg19 ref /extraspace/sli/ref/Circ2_hg19_ref_ann.txt
