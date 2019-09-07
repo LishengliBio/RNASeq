d37e10d99b8a9cf1ba717dc8989f950a996ac669
LishengliBio <35823565+LishengliBio@users.noreply.github.com>
2019-09-07 15:22:19 -0500

    Update circRNA_CircFinder_v1.0.sh

--- CircRNA/circRNA_CircFinder_v1.0.sh
+++ CircRNA/circRNA_CircFinder_v1.0.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sample=$1
-dir_alignment='/extraspace/sli/collaboration/XiangYu_circRNA/TGF_treat/alignment_ht2'
-dir_circ='/extraspace/sli/collaboration/XiangYu_circRNA/TGF_treat/circRNA/Circ'
-dir_circfinder='/extraspace/sli/collaboration/XiangYu_circRNA/TGF_treat/circRNA/CircFinder'
+dir_alignment='/extraspace/sli/circRNA01/alignment_ht2'
+dir_circ='/extraspace/sli/circRNA01/circRNA/Circ'
+dir_circfinder='/extraspace/sli/circRNA01/circRNA/CircFinder'
 mkdir -p ${dir_circfinder}/${sample}
-fa='/extraspace/sli/ref/fa/gencode_v19_genome_hg19.fa'
-gtf='/extraspace/sli/ref/gtf/gencode_v19_annotation_hg19.gtf'
+fa='/extraspace/sli/ref/fa/gencode_v28_genome.fa'
+gtf='/extraspace/sli/ref/gtf/gencode_v28_annotation.gtf'
 star='/extraspace/sli/softwares/bin/STAR'
-gdir='/extraspace/sli/ref/fa/star_index_hg19'
-unmap_fq=${dir_alignment}/${sample}/${sample}'_unmapped.fq'
+gdir='/extraspace/sli/ref/fa/star_index'
+unmap_fq=${dir_alignment}/${sample}/${sample}'_unmapped.fastq'
 ${star} --genomeDir ${gdir} --readFilesIn ${unmap_fq} --runThreadN 15 --chimSegmentMin 20 --chimScoreMin 1 --alignIntronMax 100000 \
---chimOutType Junctions SeparateSAMold --outFilterMismatchNmax 4 --alignTranscriptsPerReadNmax 100000 --outFilterMultimapNmax 2 --outFileNamePrefix ${dir_circfinder}/${sample}
+--chimOutType Junctions SeparateSAMold --outFilterMismatchNmax 4 --alignTranscriptsPerReadNmax 100000 --outFilterMultimapNmax 2 --outFileNamePrefix ${dir_circfinder}/${sample}/${sample}
 pstalign='/extraspace/sli/softwares/circRNA_finder/postProcessStarAlignment.pl'
 star_align=${dir_circfinder}/${sample}
 circ_finder_out=${dir_circfinder}/${sample}
